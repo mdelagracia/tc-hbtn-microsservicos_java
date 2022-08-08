aa7cef9574006d846974cf550ddb301fe503a05e
mdelagracia <mdelagracia@ciandt.com>
2022-08-08 11:55:22 -0300

    Task 0

---
+++ 0x03/criando_spring_boot_JPA/com/techCamps/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.techCamps.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(value = "/api-demo-jpa")
+public class DemoApplication {
+    @GetMapping("/h2-console/")
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
